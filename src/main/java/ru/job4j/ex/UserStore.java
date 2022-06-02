6508003a02010645f673f0e50f3eb24fc7b2e0d0
vsevolodgornostaev <sevlantiy@inbox.ru>
2022-06-02 21:46:43 +0300

    0.5. Иерархия исключений и множественный catch

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,42 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User rsl = null;
+        for (int i = 0; i < users.length; i++) {
+            if (login.equals(users[i].getUsername())) {
+                rsl = users[i];
+                break;
+            }
+            if (rsl == null) {
+                throw new UserNotFoundException("User is not found");
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ea) {
+            ea.printStackTrace();
+        } catch (UserNotFoundException en) {
+            en.printStackTrace();
+        }
+    }
+}
+
