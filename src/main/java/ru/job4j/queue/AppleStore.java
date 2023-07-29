43b84ba1d271b50d27ca0e911923e50512fee872
vsevolodgornostaev <sevlantiy@inbox.ru>
2023-07-30 01:08:08 +0300

    1. Интерфейс Queue

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class AppleStore {
+    public final Queue<Customer> queue;
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        String customer = null;
+        for (int i = 0; i < count; i++) {
+            customer = String.valueOf(queue.poll().getName());
+        }
+        return customer;
+    }
+
+    public String getFirstUpsetCustomer() {
+        String customer = null;
+        for (int i = 0; i <= count; i++) {
+            customer = String.valueOf(queue.poll().getName());
+        }
+        return customer;
+    }
+}
---
+++ src/main/java/ru/job4j/queue/Customer.java
@@ -0,0 +1,9 @@
+package ru.job4j.queue;
+
+public record Customer(String name, int amount) {
+
+    public String getName() {
+        return name;
+    }
+}
+
