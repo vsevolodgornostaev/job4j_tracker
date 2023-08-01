bc07deec1e29ce0c0821ba09fa875dd51bc57b9f
vsevolodgornostaev <sevlantiy@inbox.ru>
2023-08-01 07:25:34 +0300

    0. Map, HashMap

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,11 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("zeuge@gmail.com", "Vsevolod Gornostaev");
+        map.put("zeuge2018@gmail.com", "Vsevolod Gornostaev");
+        map.put("java@it.com", "Innokentiy Illarionov");
+        map.put("whereismytessarakt@avengers.com", "Loki The Hela's sun");
+        map.put("whereismytessarakt@avengers.com", "Loki The agent of Asgard");
+
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
