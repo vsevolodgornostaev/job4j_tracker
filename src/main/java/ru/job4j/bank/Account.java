500ca13b5f91145fbdb5c3fdb71baa3f6d7e1ac9
vsevolodgornostaev <sevlantiy@inbox.ru>
2023-09-09 14:30:28 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,70 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель банковского счета
+ * @author VSEVOLOD GORNOSTAEV
+ * @version 1.0
+ */
 public class Account {
+    /**
+     * Банковский счет состоит из двух параметров - банковские реквизиты и
+     * баланс денежных средств. Эти параметры хранятся в примитивных переменных
+     * типа String и double соответсвенно
+     */
     private String requisite;
     private double balance;
 
+    /**
+     * При создании банковского счета требуется указать два его параметра.
+     * Для этого создаем конструктор с входными параметрами:
+     * @param requisite - банковские реквизиты
+     * @param balance - баланс денежных средств
+     */
     public Account(String requisite, double balance) {
      this.requisite = requisite;
      this.balance = balance;
     }
 
+    /**
+     * Метод для получения значения банковских реквизитов
+     * @return возвращает значение банковских реквизитов
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод для записи банковских реквизитов
+     * @param requisite банковские реквизиты, которые указывает пользователь
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод для получения баланса денежных средств на счете
+     * @return возвращает значение денежных средств на счете
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод для записи баланса денежных средств
+     * @param balance баланс денежных средств, который указывается либо пользователем, либо
+     * может быть установлен рассчетным путём
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Переопределение метода equals. Банковские счета должны сравниваться только по
+     * номеру банковских реквизитов
+     * @param o банковский счет, который сровнивается с другим банковским счетом
+     * @return возвращает true, если банковские реквизиты идентичны, false - если нет
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +78,12 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Переопределеине метода hashcode. Поскольку метод equals был переопределен, то по такому
+     * же принципу нужно переопределить метод hashcode. То есть, если сравниваемые банковские реквизиты
+     * идентичны, то они должны иметь одинаковый уникальный целочисленный код в ячейке памяти.
+     * @return возвращет целое число типа int записанных в ячейку памяти банковских реквизитов
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,68 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель пользователя банка
+ * @author VSEVOLOD GORNOSTAEV
+ * @version 1.0
+ */
 public class User {
+    /**
+     * Для идентификации пользователя банка используются его имя и номер паспорта.
+     * Имя и номер паспорта хранятся в примитивных переменных типа String
+     */
     private String passport;
     private String username;
 
+    /**
+     * При добавлении пользователя требуется указать два его параметра.
+     * Для этого создаем конструктор с входными параметрами:
+     * @param passport - номера паспорта пользователя
+     * @param username - имя пользователя
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Метод для получения номера паспорта пользователя
+     * @return возвращает номер паспорта пользователя
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Метод для записи номера паспорта пользователя
+     * @param passport номер паспорта пользователя
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Метод для получения имени пользователя
+     * @return возвращает имя пользователя
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Метод для записи имени пользователя
+     * @param username имя пользователя
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Переопределение метода equals. Пользователи должны сравниваться только по
+     * номеру их паспорта
+     * @param o номер паспорта пользователя, который сравнивается с другим
+     * @return возвращает true, если номера паспорта идентичны, false - если нет
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +76,12 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Переопределеине метода hashcode. Поскольку метод equals был переопределен, то по такому же
+     * принципу нужно переопределить метод hashcode. То есть, если сравниваемые номера паспорта
+     * идентичны, то они должны иметь одинаковый уникальный целочисленный код в ячейке памяти.
+     * @return возвращет целое число типа int записанных в ячейку памяти номеров паспорта
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
