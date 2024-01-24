90f35180973f076bad7ad496799259dc32472124
vsevolodgornostaev <sevlantiy@inbox.ru>
2024-01-24 21:34:43 +0300

    3. Аттестация

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -53,7 +53,6 @@ public class Analyze {
                 .flatMap(pupil -> pupil.subjects().stream())
                 .collect(Collectors.groupingBy(
                         Subject::name,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::score)
                 ))
                 .entrySet().stream()
